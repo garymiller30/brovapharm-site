d764046b08241c4a7d6813f3738959d861d4a47f
garymiller30 <garymiller30@gmail.com>
2022-04-30 22:19:37 +0300

    adding trim to description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { RecoilRoot } from "recoil";
 import NextNProgress from "nextjs-progressbar";
 
+import LogRocket from "logrocket";
+LogRocket.init("gyvzco/brovapharma");
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
