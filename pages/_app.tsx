4967a2d20cb9501e46335725430a87ad3ffc6ca7
garymiller30 <garymiller30@gmail.com>
2022-05-05 00:15:18 +0300

    remove logroket

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { RecoilRoot } from "recoil";
 import NextNProgress from "nextjs-progressbar";
 
-import LogRocket from "logrocket";
-LogRocket.init("gyvzco/brovapharma");
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
