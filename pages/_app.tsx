5acbe202dbe46362b0ff44f6ea8740de9b7e2e5c
garymiller30 <garymiller30@gmail.com>
2022-05-27 17:24:14 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ScaleFade } from "@chakra-ui/react";
 import { RecoilRoot } from "recoil";
 import NextNProgress from "nextjs-progressbar";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ChakraProvider>
       <RecoilRoot>
         <NextNProgress />
-        <Component {...pageProps} />
+        <ScaleFade key={router.route} initialScale={0.9} in={true}>
+          <Component {...pageProps} />
+        </ScaleFade>
       </RecoilRoot>
     </ChakraProvider>
   );
