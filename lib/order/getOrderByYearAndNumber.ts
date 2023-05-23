f4d53d65b532b02e1c386de5be52578fb6dd60b8
garymiller30 <garymiller30@gmail.com>
2023-05-23 09:53:34 +0300

    get last number fron the same

--- lib/order/getOrderByYearAndNumber.ts
+++ lib/order/getOrderByYearAndNumber.ts
@@ -4,7 +4,7 @@ export default async function getOrderByYearAndNumber(year: number, num: number)
     try {
         const collection = await connectToDatabase();
         const filter = { Number: num, CreateDate: { $regex: `^${year}` } }
-        const order = await collection.findOne(filter);
+        const order = await collection.findOne(filter, { sort: { _id: -1 } });
         if (order)
             return JSON.parse(JSON.stringify({ ...order, id: order?._id }));
         else
